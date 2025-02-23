562261ea40541df80048aa5692d1402724d9dadc
Muhammad Amir Mohd Hastapuzi <muhd.amir.m.h@gmail.com>
2025-02-23 18:54:10 +0800

    Add overflow x hidden for contact and skills

--- vue-project/src/views/pfContact.vue
+++ vue-project/src/views/pfContact.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col gap-7 justify-center h-full">
     <p class="text-4xl lg:text-6xl">contact</p>
     <hr class="border-1"></hr>
-    <div class="overflow-auto no-scrollbar">
+    <div class="overflow-auto overflow-x-hidden no-scrollbar">
       <ul>
-        <li class="flex flex-row">
+        <li class="flex flex-row flex-wrap">
           <img
             src="/right-arrow.svg"
             alt="Right Arrow"
--- vue-project/src/views/pfSkills.vue
+++ vue-project/src/views/pfSkills.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col gap-7 justify-center h-full">
     <p class="text-4xl lg:text-6xl">skills</p>
     <hr class="border-1"></hr>
-    <div class="overflow-auto no-scrollbar">
+    <div class="overflow-x-hidden no-scrollbar">
       <ul class="flex flex-row flex-wrap">
         <li v-for="skill in skills" :key="skill.name" class="p-1 tooltip tooltip-right tooltip-info" :data-tip="skill.name">
           <img
